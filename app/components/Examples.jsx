1182ec9b23c99fc32589e26e41b4626516438a10
Sascha Michael Trinkaus <strinkaus@godaddy.com>
2016-12-08 12:37:05 +0100

    adds readme

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,9 @@
 const React = require('react');
 
-var Examples = React.createClass({
-  render: function () {
-    return (
-      <h3>Examples Component</h3>
-    )
-  }
-})
+var Examples = () => {
+  return (
+    <h3>Examples Component</h3>
+  )
+};
 
 module.exports = Examples;
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,12 +1,12 @@
 const React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function () {
-    var {temp, location} = this.props;
-    return (
-      <p>It's {temp} degress Celsius in {location}</p>
-    )
-  }
-})
+
+// arrow functions inherit 'this' from parent! so 'this.props' becomes 'props'
+// then, with the advanced destructuring, i can directly pull temp and location
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <p>It's {temp} degress Celsius in {location}</p>
+  )
+};
 
 module.exports = WeatherMessage;
